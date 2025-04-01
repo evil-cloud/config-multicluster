75b741dfe444a3dbf02846ae4df1693cd481fa2f
Jose Reynoso <reynosojose2005@gmail.com>
2025-04-01 15:03:19 -0600

    comment: mar 01 abr 2025 15:03:19 CST

--- helmfile/install-with-helm.sh
+++ helmfile/install-with-helm.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+helm repo add istio https://istio-release.storage.googleapis.com/charts
+helm repo update
+
 helm install asm-gateway . --values values/values-asm-gateway.yaml --namespace asm-gateway --debug --wait --dry-run
